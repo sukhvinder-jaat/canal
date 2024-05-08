0b31f7ef0ef631e597e8c8393dda8da5adcdf3e3
sukhvinder-jaat <monudhaka829@gmail.com>
2024-05-08 14:24:31 +0530

    turn section done

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import Hero from "./components/Hero.vue";
 import Swimming from "./components/Swimming.vue";
 import Fighting from "./components/Fighting.vue";
+import Turn from "./components/Turn.vue";
 </script>
 
 <template>
   <Hero />
   <Swimming />
   <Fighting />
+  <Turn />
 </template>
 
 <style scoped></style>
---
+++ src/components/Turn.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="max-w-[1320px] mx-auto 2xl:px-0 px-3 lg:py-[150px] py-20">
+    <p
+      class="text-lightBlack font-bold xl:text-5xl md:text-[40px] text-4xl leading-[140%] text-center mb-[50px]"
+    >
+      Turn The Tide In Minutes
+    </p>
+    <div class="relative">
+      <img
+        src="../assets/images/turn/green-icon.png"
+        alt="arrow"
+        class="md:hidden max-w-[250px] mx-auto mb-5"
+      />
+      <img
+        src="../assets/images/turn/mobile.png"
+        class="2xl:max-w-[370px] lg:max-w-[300px] md:max-w-[250px] max-w-[300px] mx-auto relative z-10"
+        alt="mobile"
+      />
+      <img
+        src="../assets/images/turn/arrow-group.png"
+        alt="arrow"
+        class="absolute bottom-[10%] md:block hidden"
+      />
+      <img
+        src="../assets/images/turn/bank.png"
+        alt="arrow"
+        class="md:hidden max-w-[250px] mx-auto mt-5"
+      />
+    </div>
+  </div>
+</template>
